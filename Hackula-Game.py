1bb06414db9937b60ff887960f5f05735424007d
victoriaa040402 <72193031+victoriaa040402@users.noreply.github.com>
2020-10-01 11:03:27 +0100

    Update Hackula-Game.py

--- Hackula-Game.py
+++ Hackula-Game.py
@@ -1,4 +1,7 @@
 #example if statements
+print("You exit the taxi and arrive at the pub in Transfervania.")
+print("You walk into the pub and see it is mostly empty. There's a bartender.")
+print("As you walk up to the bar a girl runs towards you screaming")
 statement = true
 option = int(input("1, 2 or 3"))
 while statement == True:
