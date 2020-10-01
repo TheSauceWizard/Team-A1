4160ea4b0d8e5edc47453745227d74248550bcc3
TheSauceWizard <72193189+TheSauceWizard@users.noreply.github.com>
2020-10-01 11:41:21 +0100

    Update Hackula-Game.py

--- Hackula-Game.py
+++ Hackula-Game.py
@@ -2,7 +2,7 @@
 print("You exit the taxi and arrive at the pub in Transfervania.")
 print("You walk into the pub and see it is mostly empty. There's a bartender.")
 print("As you walk up to the bar a girl runs towards you screaming")
-print("Woukd you like to:")
+print("Would you like to:")
 print("1. Listen to her.")
 print("2. Ignore her.")
 statement1 = True
@@ -13,7 +13,14 @@ while statement1 == True:
         print("You are the only one strong enough to stop him. Please, you must kill him to save our town")
         statement1 = False
     elif option1 == 2:
-        print("You ignore every word she says. The Zombie cyborgs escort her away.")
+        print("You ignore every word she says. The Cyborg Zombies escort her away.")
         statement1 = False
     else:
         statement1 = True
+print("As I look around the pub I notice these things: the bartender is standing at the bar by himself,")
+print(" the cyborg zombies have returned to the door, and an man sat in the corner of the room. ")
+print("Would you like to:")
+print("1. Speak to the bartender")
+print("2. Speak to the Cyborg Zombies")
+statement = True
+while 
