caac286cd1dfc4ba9dc1320051eb278d0dca64ff
victoriaa040402 <72193031+victoriaa040402@users.noreply.github.com>
2020-10-02 16:01:19 +0100

    Create cyborg.py

---
+++ cyborg.py
@@ -0,0 +1,16 @@
+print ("""
+As I look around the pub I notice these things: the bartender is standing at the bar by himself,
+the cyborg zombies have returned to the door, and an man sat in the corner of the room.
+""")
+print("""
+Do you:
+1. Go speak to the bartender.
+2. Go speak to the man.
+3. Go speak to the cyborg zombies.
+""")
+statement2 = True
+while statement2 == True:
+    option2 = int(input("Enter 1, 2 or 3"))
+    if option2 == 3 :
+        print("BRAINNNNNNNNNNSSSSSSSSSS")
+        statement2 = True
